1dd80a57df9f2d122634a6ca1c193dd6581b6922
michael starke <michael.starke@hicknhack-software.com>
2013-06-06 23:31:40 +0200

    Added Separator View

---
+++ HNHSeparator.m
@@ -0,0 +1,41 @@
+//
+//  HNHSeparator.m
+//
+//  Created by Michael Starke on 31.05.13.
+//
+//  Copyright (c) 2013 HicknHack Software GmbH. All rights reserved.
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+//
+
+#import "HNHSeparator.h"
+
+@implementation HNHSeparator
+
+- (void)drawRect:(NSRect)dirtyRect {
+  NSRect bounds = [self bounds];
+  [[NSColor lightGrayColor] set];
+  NSRectFill(NSMakeRect(0, 1, NSWidth(bounds), 1));
+  
+  [[NSColor whiteColor] set];
+  NSRectFill(NSMakeRect(0, 0, NSWidth(bounds), 1));
+  
+}
+
+@end
