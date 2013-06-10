fd3d932c26af597eac68c8fe38e0f43bd98530a1
michael starke <michael.starke@hicknhack-software.com>
2013-06-10 03:24:36 +0200

    Drafting for the cell

--- HNHLevelIndicatorCell.m
+++ HNHLevelIndicatorCell.m
@@ -11,6 +11,15 @@
 @implementation HNHLevelIndicatorCell
 
 - (void)drawWithFrame:(NSRect)cellFrame inView:(NSView *)controlView {
-
+  NSRect stroke = NSInsetRect(cellFrame, 0.5, 0.5);
+  [[NSColor blackColor] setStroke];
+  NSBezierPath *path = [NSBezierPath bezierPathWithRoundedRect:stroke xRadius:4 yRadius:4];
+  [path stroke];
+  NSRect pill = NSInsetRect(stroke, 1,1);
+  NSBezierPath *pillPath = [NSBezierPath bezierPathWithRoundedRect:pill xRadius:3 yRadius:3];
+  [[NSColor greenColor] setStroke];
+  [[NSColor blueColor] setFill];
+  [pillPath fill];
+  [pillPath stroke];
 }
 @end
