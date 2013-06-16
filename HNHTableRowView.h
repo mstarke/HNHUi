3434cadaf40cf2c09addf134721350c92c262b11
michael starke <michael.starke@hicknhack-software.com>
2013-06-17 01:24:49 +0200

    Added custom TableRowView for future highlighting

---
+++ HNHTableRowView.h
@@ -0,0 +1,13 @@
+//
+//  HNHTableRowView.h
+//  MacPass
+//
+//  Created by Michael Starke on 17.06.13.
+//  Copyright (c) 2013 HicknHack Software GmbH. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface HNHTableRowView : NSTableRowView
+
+@end
---
+++ HNHTableRowView.m
@@ -0,0 +1,28 @@
+//
+//  HNHTableRowView.m
+//  MacPass
+//
+//  Created by Michael Starke on 17.06.13.
+//  Copyright (c) 2013 HicknHack Software GmbH. All rights reserved.
+//
+
+#import "HNHTableRowView.h"
+
+@implementation HNHTableRowView
+
+- (id)initWithFrame:(NSRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+- (void)drawRect:(NSRect)dirtyRect
+{
+    // Drawing code here.
+}
+
+@end
