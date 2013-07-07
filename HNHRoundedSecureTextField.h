50e699699f125e8e033b9da43140c930d31c51e0
michael starke <michael.starke@hicknhack-software.com>
2013-07-07 22:06:18 +0200

    Updated Licenses

---
+++ HNHRoundedSecureTextField.h
@@ -0,0 +1,13 @@
+//
+//  HNHRoundedSecureTextField.h
+//  MacPass
+//
+//  Created by Michael Starke on 07.07.13.
+//  Copyright (c) 2013 HicknHack Software GmbH. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface HNHRoundedSecureTextField : NSSecureTextField
+
+@end
---
+++ HNHRoundedSecureTextField.m
@@ -0,0 +1,28 @@
+//
+//  HNHRoundedSecureTextField.m
+//  MacPass
+//
+//  Created by Michael Starke on 07.07.13.
+//  Copyright (c) 2013 HicknHack Software GmbH. All rights reserved.
+//
+
+#import "HNHRoundedSecureTextField.h"
+
+@implementation HNHRoundedSecureTextField
+
+- (id)initWithFrame:(NSRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+- (void)drawRect:(NSRect)dirtyRect
+{
+    // Drawing code here.
+}
+
+@end
