254906cc1096951ca077ad0bb8ca7fd955596d0c
michael starke <michael.starke@hicknhack-software.com>
2014-08-27 20:05:22 +0200

    using extern implementation

--- HNHCommon.h
+++ HNHCommon.h
@@ -6,6 +6,9 @@
 //  Copyright (c) 2014 HicknHack Software GmbH. All rights reserved.
 //
 
+#ifndef HNHUi_HNHCommon_h
+#define HNHUi_HNHCommon_h
+
 #import <AppKit/AppKit.h>
 
 /**
@@ -15,6 +18,10 @@
  *
  *  @return NSStateOn state when flag is YES, NSOffState when flag is NO
  */
-static NSInteger HNHStateForBool(BOOL flag) {
-  return flag ? NSOnState : NSOffState;
-}
+FOUNDATION_EXTERN NSInteger HNHStateForBool(BOOL flag);
+
+FOUNDATION_EXTERN BOOL HNHBoolForState(NSInteger state);
+
+FOUNDATION_EXTERN void HNHSetStateFromBool(id stateItem, BOOL isOn);
+
+#endif
